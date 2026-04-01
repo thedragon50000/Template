291905951de4d775cb5be71e05464b321e556161
Hao <thedragon50000@gmail.com>
2026-04-02 00:21:37 +0800

    fixed.

--- Assets/Samples/AI Navigation/2.0.12/Build And Connect NavMesh Surfaces/Scripts/ClickToMove.cs
+++ Assets/Samples/AI Navigation/2.0.12/Build And Connect NavMesh Surfaces/Scripts/ClickToMove.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using UnityEngine.AI;
+using UnityEngine.InputSystem;
 
 namespace Unity.AI.Navigation.Samples
 {
@@ -10,18 +11,36 @@ namespace Unity.AI.Navigation.Samples
     public class ClickToMove : MonoBehaviour
     {
         NavMeshAgent m_Agent;
-        RaycastHit m_HitInfo = new RaycastHit();
+        RaycastHit m_HitInfo;
 
-        void Start()
+        void Awake()
         {
             m_Agent = GetComponent<NavMeshAgent>();
         }
+        //
+        // void Update()
+        // {
+        //     if (Input.GetMouseButtonDown(0) && !Input.GetKey(KeyCode.LeftShift))
+        //     {
+        //         var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        //         if (Physics.Raycast(ray.origin, ray.direction, out m_HitInfo))
+        //             m_Agent.destination = m_HitInfo.point;
+        //     }
+        // }
 
-        void Update()
+        public void OnMove(InputAction.CallbackContext ctx) // 接收 Input System 訊號
         {
-            if (Input.GetMouseButtonDown(0) && !Input.GetKey(KeyCode.LeftShift))
+            if (ctx.performed)
             {
-                var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+                // 取得當前滑鼠物件
+                var mouse = Mouse.current;
+                if (mouse == null) return;
+
+                // 讀取滑鼠座標
+                Vector2 mousePosition = mouse.position.ReadValue();
+// 轉換為射線
+                Ray ray = Camera.main!.ScreenPointToRay(mousePosition);
+
                 if (Physics.Raycast(ray.origin, ray.direction, out m_HitInfo))
                     m_Agent.destination = m_HitInfo.point;
             }
